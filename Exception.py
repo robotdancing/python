504be6859eb2e6f55e2a0cdb1f70893d57f6106e
duo <18994890@qq.com>
2019-09-22 20:24:39 -0700

    exception learning

---
+++ Exception.py
@@ -0,0 +1,25 @@
+#! /usr/bin/python3
+# _*_ coding=UTF-8 _*_
+
+class MyError(Exception):
+    def __init__(self):
+        pass
+    def __str__(self):
+        return 'this is self define error'
+
+
+def exp_exception(x, y):
+    try:
+        a = x / y
+        raise MyError
+        print('a=', a)
+        return a
+    except (ZeroDivisionError, NameError, MyError) as e:
+        print(e)
+    else:
+        print('it goes as expect')
+    finally:
+        print('you will see this whatever happen')
+
+
+exp_exception(2, 9)
